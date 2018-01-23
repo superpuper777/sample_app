8b30fc55273b5235829dddebac11dc17fc75face
superpuper777 <superfevrik@gmail.com>
2018-01-23 17:18:18 +0300

    code refactoring was successful

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -7,6 +7,12 @@ require 'spec_helper'
       visit '/static_pages/home'
       expect(page).to have_content 'Sample App'
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
+
   end
 
  describe "Help page", :type => :feature  do
@@ -15,6 +21,12 @@ require 'spec_helper'
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
+
   end
 
 describe "About page", :type => :feature  do
@@ -23,5 +35,11 @@ it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
+
+it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
+    
   end
 end
