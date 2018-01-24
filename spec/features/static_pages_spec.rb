0d4921bf78ec75bd2f1feb545f249b717c7dbaf9
superpuper777 <superfevrik@gmail.com>
2018-01-24 10:31:32 +0300

    exercises except 3(chapter3)

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
  describe "Static pages", :type => :feature do
+
+let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
  describe "Home page", :type => :feature  do
 
     it "should have the content 'Sample App'" do
@@ -10,7 +13,7 @@ require 'spec_helper'
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
 
   end
@@ -24,7 +27,7 @@ require 'spec_helper'
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
 
   end
@@ -38,8 +41,23 @@ it "should have the content 'About Us'" do
 
 it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
     
   end
+
+describe "Contact", :type => :feature  do
+
+it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+    
+  end
+
 end
