9176d3fe50b764b2aaa4b416b2c9d45b86b0ee32
superpuper777 <superfevrik@gmail.com>
2018-01-28 20:48:57 +0300

    the tests in section 5.3.4 were successfully performed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,11 @@
 module ApplicationHelper
-
-  #Возвращает полный заголовок зависящий от страницы.
-  def full_title(page_title = '')  # Определение метода
-    base_title = "Ruby on Rails Tutorial Sample App" # Назначение переменной
-    if page_title.empty? #Булевой тест
-      base_title  # Явное возвчращение 
+# Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
     else
-      page_title + " | " + base_title  # Интерполяция строки
+      "#{base_title} | #{page_title}"
     end
   end
-
 end
