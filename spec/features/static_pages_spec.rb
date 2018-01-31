789c6c328680e4baa388ab2d999348b9bd95e3f1
superpuper777 <superfevrik@gmail.com>
2018-01-31 16:42:44 +0300

    Finish layout and routes

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -33,4 +33,16 @@ describe "Static pages" do
     it { should have_content('Contact') }
     it { should have_title(full_title('Contact')) }
   end
+
+  describe "User pages" do
+
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
+end
 end
