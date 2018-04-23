09b8a0c80b95895304a462470b3a9a19aae3241f
superpuper777 <superfevrik@gmail.com>
2018-04-23 13:16:08 +0300

    fixed updating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,12 @@ before_action :admin_user, only: :destroy
   def create
     @user = User.new(user_params)   
       if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      #log_in @user
+      #flash[:success] = "Welcome to the Sample App!"
+      #redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
@@ -52,13 +55,13 @@ private
 	params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end	
 
-   def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
+   #def logged_in_user
+      #unless logged_in?
+        #store_location
+        #flash[:danger] = "Please log in."
+        #redirect_to login_url
+      #end
+    #end
 
 def correct_user
   @user = User.find(params[:id])
